965970ec501920a44edfe4f00d996079f9b4b46f
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2026-03-04 15:33:16 -0300

    Adição de página 404 em que o usuário é redirecionado sempre que acessa alguma rota que não existe na aplicação

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="flex flex-1 flex-col items-center justify-center bg-white px-4 py-8">
+    <div class="flex flex-col items-center gap-6 text-center">
+      <Logo class="text-black1 h-20 w-20" />
+
+      <div class="flex flex-col gap-2">
+        <h1 class="text-black2 text-4xl font-bold">404</h1>
+
+        <p class="text-gray2 text-lg">Página não encontrada!</p>
+      </div>
+
+      <Button @click="navigateToHome">Voltar para a Home</Button>
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+import Logo from '@/components/icons/Logo.vue'
+import Button from '@/components/Button.vue'
+
+const router = useRouter()
+
+const navigateToHome = () => {
+  router.push('/')
+}
+</script>
+
+<style></style>
